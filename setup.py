fe21653a4c54db258fb602617f9b022bf0862c2c
BHAVIKA <Bhavikapathak111@gmail.com>
2023-08-10 20:29:56 +0530

    Data Ingestion

--- setup.py
+++ setup.py
@@ -1,25 +1,58 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup  # Import necessary functions from setuptools module
 from typing import List
 
-HYPEN_E_DOT = '-e .'
+HYPEN_E_DOT = '-e .'  # Define a constant variable
 
-def get_requirements(file_path:str)->List[str]:
-    requirements =[]
-    with open(file_path) as file_obj:
-        requirements=file_obj.readlines()
-        requirements=[req.replace("\n","") for req in requirements]
+def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function retrieves the requirements from a file and returns them as a list.
+    '''
+    requirements = []  # Initialize an empty list to store requirements
+    with open(file_path) as file_obj:  # Open the specified file
+        requirements = file_obj.readlines()  # Read all lines from the file
+        requirements = [req.replace('\n', '') for req in requirements]  # Remove newline characters
 
-        if HYPEN_E_DOT in requirements:
-            requirements.remove(HYPEN_E_DOT)
-
-        return requirements
+        if HYPEN_E_DOT in requirements:  # Check if '-e .' is present in requirements
+            requirements.remove(HYPEN_E_DOT)  # Remove '-e .' from the list
+    return requirements  # Return the list of requirements
 
 
 setup(
-    name='DiamondPricePredection',
-    version='0.0.1',
-    author='Bhavika',
-    author_email='bhavikapathak111@gmail.com',
-    install_requires=get_requirements('requirements.txt'),
-    package=find_packages()
+    name='mlproject',  # Specify the name of the package
+    version='0.0.1',  # Specify the version of the package
+    author='bhavika-pathak',  # Specify the author of the package
+    author_email='bhavikapathak111@gmail.com',  # Specify the author's email
+    packages=find_packages(),  # Automatically find all packages and modules in the current directory
+    install_requires=get_requirements('requirements.txt'),  # Retrieve requirements from 'requirements.txt' file
 )
+
+
+"""
+The code you provided is a Python script that sets up a package using setuptools. Let's go through the code and explain each part:
+
+from setuptools import find_packages, setup: This line imports the necessary functions from the setuptools module. find_packages is used to locate all packages and modules in the current directory, while setup is a function used to configure the package.
+
+HYPEN_E_DOT = '-e .': This line defines a constant variable HYPEN_E_DOT and assigns it the value "-e .".
+
+def get_requirements(file_path:str)->List[str]:: This line defines a function named get_requirements that takes a file path as an argument and returns a list of strings.
+
+requirements=[]: This line initializes an empty list called requirements.
+
+with open(file_path) as file_obj:: This line opens the file specified by file_path in a context manager, allowing us to read its contents.
+
+requirements=file_obj.readlines(): This line reads all the lines from the file and assigns them to the requirements variable as a list.
+
+requirements = [req.replace('\n','') for req in requirements]: This line uses a list comprehension to remove the newline character ('\n') from each line in the requirements list.
+
+if HYPEN_E_DOT in requirements: requirements.remove(HYPEN_E_DOT): This conditional statement checks if the HYPEN_E_DOT string is present in the requirements list. If it is, it removes it from the list.
+
+return requirements: This line returns the requirements list.
+
+setup(...): This line sets up the package configuration using the setup function from setuptools. Here are the arguments passed to the function:
+
+name='mlproject': The name of the package.
+version='0.0.1': The version of the package.
+author='ayush-pujari-07': The name of the package author.
+author_email='ayush08.pujari@gmail.com': The email address of the package author.
+packages=find_packages(): Automatically find all packages and modules in the current directory.
+install_requires=get_requirements('requirements.txt'): Retrieve the requirements from the 'requirements.txt' file using the get_requirements function and specify them as the package's installation dependencies."""
